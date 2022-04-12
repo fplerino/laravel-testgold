47c8d292bc7ff5407a340efa5214e8f7fc3f3b26
Rino <85146899+fplerino@users.noreply.github.com>
2022-04-12 22:29:40 +0800

    Add files via upload

---
+++ src/Providers/Gg.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tete\gold\Providers;
+
+class Gg {
+	
+	public static function eachoworld(){
+		return "hello world";
+	}
+}
---
+++ src/Providers/GgServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tete\gold\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class GgServiceProvider extends ServiceProvider {
+
+    public function boot() {
+        
+    }
+
+    public function register() {
+        
+    }
+
+}
